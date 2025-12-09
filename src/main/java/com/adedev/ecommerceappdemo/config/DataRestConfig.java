ff104bee9caa3a3862a48d6da688b57cfca98ade
Adelina Apetrei <adelina.apetrei26@gmail.com>
2025-12-09 13:28:16 +0200

    Disable http methods

---
+++ src/main/java/com/adedev/ecommerceappdemo/config/DataRestConfig.java
@@ -0,0 +1,30 @@
+package com.adedev.ecommerceappdemo.config;
+
+import com.adedev.ecommerceappdemo.domain.Product;
+import com.adedev.ecommerceappdemo.domain.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable HTTP methods for Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        //disable HTTP methods for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
--- src/main/java/com/adedev/ecommerceappdemo/repository/ProductRepository.java
+++ src/main/java/com/adedev/ecommerceappdemo/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package com.adedev.ecommerceappdemo.repository;
 
 import com.adedev.ecommerceappdemo.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(collectionResourceRel = "product", path = "product")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
