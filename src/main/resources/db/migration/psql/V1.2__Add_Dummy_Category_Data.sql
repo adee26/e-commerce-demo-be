f05e61bd62702b8be1643b6e4cc7c25c579db163
Adelina Apetrei <adelina.apetrei26@gmail.com>
2025-12-10 23:55:49 +0200

    Create searchByCategory logic

---
+++ src/main/resources/db/migration/psql/V1.2__Add_Dummy_Category_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO product_category(category_name) VALUES ('Books');
+INSERT INTO product_category(category_name) VALUES ('Coffee Mugs');
+INSERT INTO product_category(category_name) VALUES ('Mouse Pads');
+INSERT INTO product_category(category_name) VALUES ('Luggage Tags');
