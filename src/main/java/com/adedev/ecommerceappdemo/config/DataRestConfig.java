cbaa35e59a6790d875a4ef275e483f2d9385867d
Adelina Apetrei <adelina.apetrei26@gmail.com>
2025-12-11 15:28:46 +0200

    Expose internal ids

--- src/main/java/com/adedev/ecommerceappdemo/config/DataRestConfig.java
+++ src/main/java/com/adedev/ecommerceappdemo/config/DataRestConfig.java
@@ -2,14 +2,26 @@ package com.adedev.ecommerceappdemo.config;
 
 import com.adedev.ecommerceappdemo.domain.Product;
 import com.adedev.ecommerceappdemo.domain.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +38,21 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        //expose the internal ids
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration configuration) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        configuration.exposeIdsFor(domainTypes);
     }
 }
